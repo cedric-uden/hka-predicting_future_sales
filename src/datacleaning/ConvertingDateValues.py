37bcb27236b218958a53df44a58c90f9471e64f5
Cédric Uden <cedric@uden.de>
2021-09-03 16:26:25 +0200

    added iso8601 conversion function

--- src/datacleaning/ConvertingDateValues.py
+++ src/datacleaning/ConvertingDateValues.py
@@ -1,3 +1,4 @@
+iso8601_format = '%Y-%m-%d'
 
 
 def date_to_month(df):
@@ -6,4 +7,13 @@ def date_to_month(df):
     Transform said values to a monthly interval.
     """
     df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m'))
+    return df
+
+
+def date_to_iso8601(df):
+    """
+    Pass a dataframe with a column "date" containing daily values.
+    Transform said values to an ISO8601 matching format.
+    """
+    df['date'] = df['date'].apply(lambda x: x.strftime(iso8601_format))
     return df
