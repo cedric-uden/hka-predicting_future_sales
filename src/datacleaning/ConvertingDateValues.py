f305382342982691297175400364c2f3fe14a9c4
Cédric Uden <cedric@uden.de>
2021-09-03 15:51:44 +0200

    parsing date for datetime at import now

--- src/datacleaning/ConvertingDateValues.py
+++ src/datacleaning/ConvertingDateValues.py
@@ -6,6 +6,5 @@ def date_to_month(df):
     Pass a dataframe with a column "date" containing daily values.
     Transform said values to a monthly interval.
     """
-    df['date'] = pd.to_datetime(df['date'], dayfirst=True)
     df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m'))
     return df
