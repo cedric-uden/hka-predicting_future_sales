0dce3dd29b9acb8606229389d40a52254816a0b0
Cédric Uden <cedric@uden.de>
2021-09-15 07:47:46 +0200

    formatting for graphs

--- src/plotting/Defaults.py
+++ src/plotting/Defaults.py
@@ -12,8 +12,22 @@ def set_defaults():
     This method helps to maintain a uniform look across the project.
     """
 
+    # rcParams documentation: https://matplotlib.org/stable/tutorials/introductory/customizing.html
+
     plt.rcParams['lines.color'] = iwi_purple  # override acf, pacf colors
     plt.rcParams['lines.markersize'] = 4
     plt.rcParams['lines.markerfacecolor'] = hka_red
     plt.rcParams['figure.figsize'] = (30,10)
-    plt.rcParams['legend.fontsize'] = 16
+    plt.rcParams['legend.fontsize'] = 34
+    plt.rcParams['axes.titleweight'] = 'extra bold'
+    plt.rcParams['axes.labelweight'] = 'bold'
+
+    plt.rcParams['xtick.direction'] = 'inout'
+
+    # spacing options
+    plt.rcParams['font.size'] = 30
+    plt.rcParams['axes.titlepad'] = 15
+    plt.rcParams['axes.labelpad'] = 20
+    plt.rcParams['xtick.major.pad'] = 10
+    plt.rcParams['ytick.major.pad'] = 10
+    plt.rcParams['legend.borderaxespad'] = 1
