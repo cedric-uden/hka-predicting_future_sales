c706729eba8123cf68bf0291d8298fb1edb7eabc
Cédric Uden <cedric@uden.de>
2021-09-15 08:55:54 +0200

    revisited the code snippet

--- report/external_content/code/lag_features.py
+++ report/external_content/code/lag_features.py
@@ -1,5 +1,6 @@
 def compute_lag_feature(df, lags, feature):
-    for i in lags: # id_columns = ['date_block_num', 'shop_id', 'item_id']
+    id_columns = ['date_block_num', 'shop_id', 'item_id']
+    for i in lags:
         shifted = df[cl(id_columns, feature)].copy()
         shifted.columns = cl(id_columns, f"{feature}_lag_{str(i)}")
         shifted['date_block_num'] += i
