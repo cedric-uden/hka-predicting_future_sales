d5e8dcc89e0439bfef4f1084f5c3ffd4113bf21b
Cédric Uden <cedric@uden.de>
2021-09-15 15:56:49 +0200

    first proof reading run complete

--- report/external_content/code/lag_features.py
+++ report/external_content/code/lag_features.py
@@ -1,9 +1,9 @@
-def compute_lag_feature(df, lags, feature):
+def compute_lag_feature(df, lags, features):
     id_columns = ['date_block_num', 'shop_id', 'item_id']
     # N.B. `cl` concatenates elements from a lists with other elements
-    for i in lags:
-        shifted = df[cl(id_columns, feature)].copy()
-        shifted.columns = cl(id_columns, f"{feature}_lag_{str(i)}")
-        shifted['date_block_num'] += i
+    for n in lags:
+        shifted = df[cl(id_columns, features)].copy()
+        shifted.columns = cl(id_columns, f"{features}_lag_{str(n)}")
+        shifted['date_block_num'] += n
         df = pd.merge(df, shifted, on=id_columns, how='left')
     return df
